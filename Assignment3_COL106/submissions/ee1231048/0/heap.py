287a5723e537bd0c713fba197062315624d0544c
jayesh-narayanan <jayeshny@gmail.com>
2024-11-27 21:42:54 +0530

    Update heap.py

--- Assignment3_COL106/submissions/ee1231048/0/heap.py
+++ Assignment3_COL106/submissions/ee1231048/0/heap.py
@@ -17,73 +17,56 @@ def comp_arrival_time(node1,node2):
 
 
 class Heap:
-    def __init__(self, comparison_function):
-        '''
-        Initializes the heap with a comparator function.
-        Arguments:
-            comparison_function: function : A function to compare two elements
-        '''
-        self.comparison_function = comparison_function
+    def _init_(self, comparison_function, init_array):
         self.heap = []
+        self.compare = comparison_function
+        for value in init_array:
+            self.insert(value)
+    def is_empty(self):
+        return len(self.heap) == 0
 
-    def _heapify_up(self, index):
-        '''
-        Helper function to maintain heap properties after insertion.
-        '''
-        parent_index = (index - 1) // 2
-        if index > 0 and self.comparison_function(self.heap[index], self.heap[parent_index]):
-            # Swap if the current node violates heap property
-            self.heap[index], self.heap[parent_index] = self.heap[parent_index], self.heap[index]
-            self._heapify_up(parent_index)
+    def left(self, index):
+        return 2 * index + 1
+    def right(self, index):
+        return 2 * index + 2
+    
+    def has_left(self, index):
+        return self.left(index) < len(self.heap)
+    def has_right(self, index):
+        return self.right(index) < len(self.heap)
 
-    def _heapify_down(self, index):
-        '''
-        Helper function to maintain heap properties after extraction.
-        '''
-        smallest = index
-        left_child_index = 2 * index + 1
-        right_child_index = 2 * index + 2
+    def down_heap(self, index):
+        if (2 * index + 1) < len(self.heap):
+            left = 2 * index + 1
+            small_child = left
+            if (2 * index + 2) < len(self.heap):
+                right = 2 * index + 2
+                if self.compare(self.heap[right], self.heap[left]):
+                    small_child = right
+            if self.compare(self.heap[small_child], self.heap[index]):
+                self.heap[index], self.heap[small_child] = self.heap[small_child], self.heap[index]
+                self.down_heap(small_child)
 
-        if left_child_index < len(self.heap) and self.comparison_function(self.heap[left_child_index], self.heap[smallest]):
-            smallest = left_child_index
-
-        if right_child_index < len(self.heap) and self.comparison_function(self.heap[right_child_index], self.heap[smallest]):
-            smallest = right_child_index
-
-        if smallest != index:
-            # Swap and recursively heapify
-            self.heap[index], self.heap[smallest] = self.heap[smallest], self.heap[index]
-            self._heapify_down(smallest)
-
+    def up_heap(self, index):
+        parent = (index - 1) // 2
+        if index > 0 and self.compare(self.heap[index], self.heap[parent]):
+            self.heap[index], self.heap[parent] = self.heap[parent], self.heap[index]
+            self.up_heap(parent)
+            
     def insert(self, value):
-        '''
-        Inserts a value into the heap.
-        '''
         self.heap.append(value)
-        self._heapify_up(len(self.heap) - 1)
+        self.up_heap(len(self.heap) - 1)
 
     def extract(self):
-        '''
-        Removes and returns the top of the heap.
-        '''
-        if not self.heap:
-            raise IndexError("Heap is empty")
-        top_value = self.heap[0]
-        # Replace root with the last element
-        self.heap[0] = self.heap[-1]
-        self.heap.pop()
-        # Restore heap property
-        if self.heap:
-            self._heapify_down(0)
-        return top_value
+        if len(self.heap) == 0:
+            return None
+        self.heap[0], self.heap[-1] = self.heap[-1], self.heap[0]
+        item = self.heap.pop()
+        self.down_heap(0)
+	return item
 
     def top(self):
-        '''
-        Returns the top value of the heap without removing it.
-        '''
-        if not self.heap:
-            raise IndexError("Heap is empty")
-        return self.heap[0]
+        return self.heap[0] if not self.is_empty() else None
 
 class Heap_crew:
   '''
